6dcfe25a2ff6494e7f40ee3ec623dc8acc43671c
eka-septian <118880703+eka-septian@users.noreply.github.com>
2024-09-02 13:40:16 +0700

    feat: parse identifier expression

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -15,10 +15,32 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns map[token.TokenType]infixParseFn
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l, errors: []string{}}
+
+    p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.IDENT, p.parseIdentifier)
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -58,7 +80,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -98,6 +120,30 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+    stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+    prefix := p.prefixParseFns[p.curToken.Type]
+    if prefix == nil {
+        return nil
+    }
+    leftExp := prefix()
+    return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+    return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
@@ -119,4 +165,12 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
--- internal/parser/perser_test.go
+++ internal/parser/perser_test.go
@@ -55,19 +55,48 @@ func TestReturnStatement(t *testing.T) {
 	program := p.ParseProgram()
 	checkParseErrors(t, p)
 
-    if len(program.Statements) != 3 {
+	if len(program.Statements) != 3 {
 		t.Fatalf("program.Statements doues not contain 3 statements, got=%d", len(program.Statements))
-    }
+	}
 
-    for _, stmt := range program.Statements {
-        returnStmt, ok := stmt.(*ast.ReturnStatement)
-        if !ok {
-            t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
-            continue
-        }
-        if returnStmt.TokenLiteral() != "return" {
-            t.Errorf("returnStmt.TokenLiteral not 'return'. got=%T", returnStmt.TokenLiteral())
-        }
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return'. got=%T", returnStmt.TokenLiteral())
+		}
+	}
+}
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements, got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement, got=%T", program.Statements[0])
+	}
+
+    ident, ok := stmt.Expression.(*ast.Identifier)
+    if !ok {
+		t.Fatalf("exp not *ast.Identifier, got=%T", stmt.Expression)
+    }
+    if ident.Value != "foobar" {
+		t.Fatalf("ident..Value not %s, got=%s", "foobar", ident.Value)
+    }
+    if ident.TokenLiteral() != "foobar" {
+		t.Fatalf("ident..TokenLiteral not %s, got=%s", "foobar", ident.TokenLiteral())
     }
 }
 
